3e42ea720d4994fbb6190955c9f56c8c853fa12f
MarinaToptygina <91845711+MarinaToptygina@users.noreply.github.com>
2021-12-27 21:33:42 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,20 +1,29 @@
-#include<iostream>
-#include<string>
-#include<stdio.h>
-
-using namespace std;
-string replacement_extension(string fname, string extension) {
-	int n;
-	n = fname.find('.');
-	fname = fname.substr(0, n) + extension;
-	cout << fname << endl;
-	return fname;
-}
-
-int main() {
-	string fileName, newextension;
-	getline(cin, fileName);
-	getline(cin, newextension);
-	replacement_extension(fileName, newextension);
-	return 0;
-}
+#include<iostream>			
+#include<string> 		
+#include<stdio.h>			
+using namespace std;			
+char replacement_extension(char fName[100], char extension[10]) {			
+	int i = 0;		
+			
+	while (fName[i] != '.') {		
+		if (fName[i] == '\0')   	
+			break;            
+		i++;	
+	}		
+	if (fName[i] == '.')	           
+		strcpy_s(fName + i, 100, extension);  
+	else strcat_s(fName, 100, extension);    		
+			
+	cout << fName << endl;		
+	return 0;		
+}			
+			
+int main() {			
+			
+	char filename[100], newextension[10];		
+	cin.getline(filename, 100);		
+	cin.getline(newextension, 10);		
+	replacement_extension(filename, newextension);		
+			
+	return 0;		
+}			
