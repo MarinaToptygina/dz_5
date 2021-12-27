68898199210b7a159a055cc75f0de71512f1ec82
MarinaToptygina <91845711+MarinaToptygina@users.noreply.github.com>
2021-12-27 21:36:08 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,18 +1,19 @@
 #include<iostream>			
-#include<string> 		
+#include<string> 			
+			
 #include<stdio.h>			
 using namespace std;			
 char replacement_extension(char fName[100], char extension[10]) {			
 	int i = 0;		
 			
 	while (fName[i] != '.') {		
-		if (fName[i] == '\0')   	
-			break;            
+		if (fName[i] == '\0') 	
+			break;             
 		i++;	
 	}		
-	if (fName[i] == '.')	           
-		strcpy_s(fName + i, 100, extension);  
-	else strcat_s(fName, 100, extension);    		
+	if (fName[i] == '.')	            
+		strcpy_s(fName + i, 100, extension);  	
+	else strcat_s(fName, 100, extension);      		
 			
 	cout << fName << endl;		
 	return 0;		
